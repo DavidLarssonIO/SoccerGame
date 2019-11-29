26d49a6e1667c2f5e02d7dfdea088213cb520a65
linan10 <sclinus@student.chalmers.se>
2019-11-29 16:23:27 +0100

    hej

--- PlotField.m
+++ PlotField.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [] = PlotField(field,time,subFieldSize)
 [w,l] = size(field);
 imagesc((1:w)+0.5, (1:l)+0.5,field);
@@ -20,27 +19,4 @@ str = string(num);
 yticklabels(str);
 drawnow
 end
-%hej hopp 2
-=======
-function [] = PlotField(field,time,subFieldSize) %Bra jobbbat!
-[w,l] = size(field);
-imagesc((1:w)+0.5, (1:l)+0.5,field);
-axis equal
-set(gca, 'XTick', 1:subFieldSize(1):w+1, 'YTick', 1:subFieldSize(2):l+1, ...
-    'XLim', [1 w+1], 'YLim', [1 l+1], ...
-    'GridLineStyle', '-', 'XGrid', 'on', 'YGrid', 'on');
-title(['$t = $ ' num2str(time)],'Interpreter','latex')
-map = [ 1 1 1
-        0 0 0
-        1 1 0];
-colormap(map);
-caxis([0 3]);
-num = 0:subFieldSize(1):l;
-str = string(num);
-xticklabels(str);
-num = 0:subFieldSize(2):l;
-str = string(num);
-yticklabels(str);
-drawnow
-end
->>>>>>> a4cc336382a2b8e963e2a55b9861cba9f945a9ff
+
