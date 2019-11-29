0f5d99be585d6d4d30f7c49430b6f8e061e3aae0
DavidLarssonIO <58039481+DavidLarssonIO@users.noreply.github.com>
2019-11-29 16:18:14 +0100

    Update GenerateField.m

--- GenerateField.m
+++ GenerateField.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function field = GenerateField(nPlayers,subFieldSize)
     XPlayers = nPlayers(1);
     YPlayers = nPlayers(2);
@@ -6,12 +5,3 @@ function field = GenerateField(nPlayers,subFieldSize)
     ySubFieldSize = subFieldSize(2);
     field = zeros(XPlayers*xSubFieldSize,YPlayers*ySubFieldSize);
 end
-=======
-function field = GenerateField(nPlayers,subFieldSize)
-    XPlayers = nPlayers(1);
-    YPlayers = nPlayers(2);
-    xSubFieldSize = subFieldSize(1);
-    ySubFieldSize = subFieldSize(2);
-    field = zeros(XPlayers*xSubFieldSize,YPlayers*ySubFieldSize);
-end%Hej
->>>>>>> a4cc336382a2b8e963e2a55b9861cba9f945a9ff
