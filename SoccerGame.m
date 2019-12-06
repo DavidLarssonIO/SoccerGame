62c891a9ef8016c6e177523d1277a456c735b5b3
Administratör <davidlarsson97@gmail.com>
2019-12-06 09:59:45 +0100

    Added comments

--- SoccerGame.m
+++ SoccerGame.m
@@ -2,8 +2,8 @@
 
 clc, clf, clear
 time = 0;
-nPlayers = [3 3];
-subFieldSize = [2 2];
+nPlayers = [3 3]; % 9 players in 3x3 grid
+subFieldSize = [2 2]; % Every player have 2x2 as their zone
 field = GenerateField(nPlayers,subFieldSize);
 field = eye(size(field));
 PlotField(field,time,subFieldSize)
