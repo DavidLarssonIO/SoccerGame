957e88da10dd486d8c18a21caf83ec4e4f7548f8
MarcChalm <marcholm@student.chalmers.se>
2019-12-06 15:20:24 +0100

    Restructured the input for UpdateField since it only need positions

--- DataManupliation/DynamicBehaviors/Update.m
+++ DataManupliation/DynamicBehaviors/Update.m
@@ -2,7 +2,11 @@ function [field] = Update(players, ball, timeSync, timeDelta)
 % This function updated all the states of all the players and the field
 
 [updatedPlayers, updatedBall] = UpdatePlayers(players, ball, timeDelta);
-updatedField = UpdateField(updatedPlayers, updatedBall);
+
+updatedPlayersPositions = updatedPlayers(1,:,:);
+updatedBallPosition = updatedBall(1,:,:);
+
+updatedField = UpdateField(updatedPlayersPositions, updatedBallPosition);
 pause(timeSync);
 field = updatedField;
 
