bc2c35f9e2434f24ea1e3e7dc0d84c5b37af5f09
Administratör <davidlarsson97@gmail.com>
2019-12-09 08:46:04 +0100

    Added in SoccerGame the cont plot

--- SoccerGame.m
+++ SoccerGame.m
@@ -1,9 +1,13 @@
 %% main
 
-clc, clf, clear
+clc, clear, figure(1), clf 
 time = 0;
 nPlayers = [3 3]; % 9 players in 3x3 grid
 subFieldSize = [2 2]; % Every player have 2x2 as their zone
 field = GenerateField(nPlayers,subFieldSize);
 field = eye(size(field));
 PlotField(field,time,subFieldSize)
+
+figure(2), clf
+fieldSize = [120 90];
+PlotContField(fieldSize)
